b2eb845da27f223375b32f3fe2a50d8d01cce2b5
michael <mvanderlyn27@gmail.com>
2024-12-08 19:51:48 -0500

    fixed image background

--- src/components/screens/SplashScreen.tsx
+++ src/components/screens/SplashScreen.tsx
@@ -10,20 +10,22 @@ const StyledText = styled(Text);
 const StyledSafeAreaView = styled(SafeAreaView);
 export default function LoadingScreen() {
   return (
-    <StyledSafeAreaView className="absolute top-0 left-0 right-0 bottom-0">
-      <StyledText
-        className="text-xl tracking-widest text-secondary text-center"
-        style={{ fontFamily: "PragmaticaExtended-bold" }}>
-        SLICE OF LIFE
-      </StyledText>
-      <StyledText
-        className="text-[10px] tracking-widest text-secondary text-center"
-        style={{ fontFamily: "PragmaticaExtended" }}>
-        ORDINARY. MUNDANE. BUT UNIQUELY YOURS.
-      </StyledText>
-      <StyledMotiView className="absolute top-0 bottom-0 right-0 left-0  justify-center items-center">
-        <ActivityIndicator size="large" color="white" />
-      </StyledMotiView>
-    </StyledSafeAreaView>
+    <ImageBackground style={{ flex: 1 }} source={getImageFromPath("bg_03")}>
+      <StyledSafeAreaView className="absolute top-0 left-0 right-0 bottom-0">
+        <StyledText
+          className="text-xl tracking-widest text-secondary text-center"
+          style={{ fontFamily: "PragmaticaExtended-bold" }}>
+          SLICE OF LIFE
+        </StyledText>
+        <StyledText
+          className="text-[10px] tracking-widest text-secondary text-center"
+          style={{ fontFamily: "PragmaticaExtended" }}>
+          ORDINARY. MUNDANE. BUT UNIQUELY YOURS.
+        </StyledText>
+        <StyledMotiView className="absolute top-0 bottom-0 right-0 left-0  justify-center items-center">
+          <ActivityIndicator size="large" color="white" />
+        </StyledMotiView>
+      </StyledSafeAreaView>
+    </ImageBackground>
   );
 }
